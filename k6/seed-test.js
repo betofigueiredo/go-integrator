b8a44e13c6032b3325c80319245b37f89683c38d
Beto <beto@2sidesof1.com>
2024-10-19 21:17:42 -0300

    feat: add more info to user when creating with k6

--- k6/seed-test.js
+++ k6/seed-test.js
@@ -11,20 +11,28 @@ export let options = {
   ],
 };
 
-const generateName = () => {
+const generateUserInfo = (idx) => {
+  const sexes = ["MALE", "FEMALE"];
+  const roles = ["ADMIN", "MANAGER", "CONTRIBUTOR"];
+  const actives = [true, false];
   const firstName = namesList[Math.floor(Math.random() * namesList.length)];
   const lastName = namesList[Math.floor(Math.random() * namesList.length)];
-  return `${firstName} ${lastName}`;
+  const name = `${firstName} ${lastName}`;
+  return {
+    name: name,
+    email: `${name.toLowerCase().replace(" ", "")}@mail${idx}.com`,
+    phone: String(Math.floor(Math.random() * 1000000000)),
+    sex: sexes[Math.floor(Math.random() * sexes.length)],
+    birth_date: "1990-01-01",
+    role: roles[Math.floor(Math.random() * roles.length)],
+    is_active: actives[Math.floor(Math.random() * actives.length)],
+  };
 };
 
 const createBody = new SharedArray("body", () => {
   const bodies = [];
   for (let i = 0; i < 100; i++) {
-    const name = generateName();
-    const newBody = {
-      name,
-      email: `${name.toLowerCase().replace(" ", "")}@mail${i}.com`,
-    };
+    const newBody = generateUserInfo(i);
     bodies.push(newBody);
   }
   return bodies;
